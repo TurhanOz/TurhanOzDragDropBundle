3b15efe4b149c85412922e4e00d03ed9115b05eb
TurhanTech <sir.turhan@gmail.com>
2013-03-22 19:23:59 +0100

    - Car CRUD (thanks to php app/console generate:doctrine:crud)

---
+++ Controller/CarController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace TurhanOz\DragDropBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use TurhanOz\DragDropBundle\Entity\Car;
+use TurhanOz\DragDropBundle\Form\CarType;
+
+/**
+ * Car controller.
+ *
+ * @Route("/car")
+ */
+class CarController extends Controller
+{
+    /**
+     * Lists all Car entities.
+     *
+     * @Route("/", name="car")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('TurhanOzDragDropBundle:Car')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Car entity.
+     *
+     * @Route("/", name="car_create")
+     * @Method("POST")
+     * @Template("TurhanOzDragDropBundle:Car:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Car();
+        $form = $this->createForm(new CarType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('car_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Car entity.
+     *
+     * @Route("/new", name="car_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Car();
+        $form   = $this->createForm(new CarType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Car entity.
+     *
+     * @Route("/{id}", name="car_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TurhanOzDragDropBundle:Car')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Car entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Car entity.
+     *
+     * @Route("/{id}/edit", name="car_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TurhanOzDragDropBundle:Car')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Car entity.');
+        }
+
+        $editForm = $this->createForm(new CarType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Car entity.
+     *
+     * @Route("/{id}", name="car_update")
+     * @Method("PUT")
+     * @Template("TurhanOzDragDropBundle:Car:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('TurhanOzDragDropBundle:Car')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Car entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new CarType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('car_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Car entity.
+     *
+     * @Route("/{id}", name="car_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('TurhanOzDragDropBundle:Car')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Car entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('car'));
+    }
+
+    /**
+     * Creates a form to delete a Car entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
